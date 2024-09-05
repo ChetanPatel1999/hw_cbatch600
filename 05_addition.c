8c7d9735fd5958dad796f72eb8c65e06183c5c35
ChetanPatel1999 <chetan8085patel@gmail.com>
2024-09-05 10:48:19 +0530

    04/09

---
+++ 05_addition.c
@@ -0,0 +1,13 @@
+// write a programm to add two numbers.
+#include <stdio.h>
+void main()
+{
+    int a, b, c;
+    printf("Addition programm....\n");
+    a = 12;
+    b = 5;
+    printf("value of a = %d\n", a);
+    printf("value of b = %d\n", b);
+    c=a+b;
+    printf("sum of %d and %d = %d",a,b,c);
+}
