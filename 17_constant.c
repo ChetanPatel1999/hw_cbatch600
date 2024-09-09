39a0d9e77688a574b60ac742d82f0ebe0070f18c
ChetanPatel1999 <chetan8085patel@gmail.com>
2024-09-09 19:21:18 +0530

    09/09

---
+++ 17_constant.c
@@ -0,0 +1,12 @@
+//constatnt variable example
+//wap to print area of circle.
+#include<stdio.h>
+void main()
+{
+	const float pi=3.141;
+	float radius,area;
+    radius=32.45;
+	area=pi*radius*radius;
+	printf("area of circle : %.2f",area);
+		
+}
---
+++ 18_asccii.c
@@ -0,0 +1,9 @@
+// printf ascci number for a charcter
+#include<stdio.h>
+void main()
+{
+	char ch='A';
+	int ascii=3;
+	printf("ascii code of %c = %d\n",ch,ch);
+	printf("charcter  of %d = %c\n",ascii,ascii);	
+}
