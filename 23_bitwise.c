a1d66a810aa0c188595a66b27181282a3bf77de9
ChetanPatel1999 <chetan8085patel@gmail.com>
2024-09-14 18:56:01 +0530

    14/09

---
+++ 23_bitwise.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+    int a = -51, b = 2, ans;
+    // ans=a&b;
+    //  ans=a|b;
+    // ans = a ^ b;
+    // ans = a << b;
+    // ans = a >> b;
+      ans =~a;
+    printf("ans = %d \n", ans);
+}
